b8394fbe46bc912ba0bcabbee1e9d0a58f4b3187
Avi Balsingh <lemonlimewine@gmail.com>
2020-07-27 21:34:21 -0400

    Finished server file, having error

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ async function mainApp(){
     ])
 
     if( response.action=="employee"){
-        let employeeList = await.db.query(
+        let employeeList = await db.query(
             "SELECT e.id," + 
             "CONCAT(e.first_name,' ',e.last_name) AS employeeName,"+
             "CONCAT(m.first_name,' ',m.last_name) AS managerName,r.title,r.salary "+
@@ -93,7 +93,160 @@ async function mainApp(){
             mainApp()
 
         }
+        if( response.action=="update"){
+            let employeeNames = []
+            employeeList.forEach( (item) =>{
+                employeeNames.push( { name:item.employeeName, value:item.id } )
+            })
 
+            const dbRole = await db.query( "SELECT * FROM role")
+            role = []
+            dbRole.forEach( (item) => {
+                role.push( { name: item.title, value: item.id } )
+            })
 
+            response = await inquirer.prompt([
+                { message: "Which employee do you want to modify?", type:"list", name:"modifyEmployee",
+                    choices: employeeNames },
+                { message: "What is his/her new role?", type:"list", name:"newRole",
+                    choices: role }
+            ])
+            // console.log( `UPDATE employee SET role_id=${response.newRole} WHERE id=${response.modifyEmployee} `)
+
+            let updateRole = await db.query( `UPDATE employee SET role_id=${response.newRole} WHERE id=${response.modifyEmployee} `)
+            console.log( `Update successful.`)
+            mainApp()
+        }
+       
+        if( response.action=="remove"){
+            let employeeNames = []
+            employeeList.forEach( (item) =>{
+                employeeNames.push( { name:item.employeeName, value:item.id } )
+            })
+
+            response = await inquirer.prompt([
+                { message: "Which employee do you want to remove?", type:"list", name:"deleteEmployee",
+                    choices: employeeNames }
+            ])
+
+            let deleteRole = await db.query( `DELETE FROM employee WHERE id='${response.deleteEmployee}'` )
+           
+            console.log( `Employee has been removed.`)
+            mainApp()
+        }
+
+        if( response.action=="return" ){
+            console.log( `Returning to the main menu...`)
+            mainApp()
+        }
     }
+
+    if( response.action=="department" ){
+        //Display department list
+        let departmentList = await db.query( "SELECT * FROM department" )
+        console.table( departmentList )
+
+        //Prompt user for further action for department info
+        response = await inquirer.prompt([
+            { message:"What do you want to do now?", type:"list", name:"action", 
+            choices:[
+                { name: "Add a department", value: "add" },
+                { name: "Remove a department", value: "remove" },
+                { name: "Return to main menu", value: "return" }
+            ]}
+        ])
+
+        //Add new department to database
+        if( response.action == "add" ){
+            response = await inquirer.prompt([
+                { message: "What is the name of new department?", type:"input", name:"createDep" }
+            ])
+        
+            let newDep = await db.query( "INSERT INTO department VALUES( ?,? ) ", 
+            [ 0, response.createDep ] )
+            
+            console.log( `Department ${response.createDep} has been added to database.`)
+            mainApp()
+        }
+
+        //Remove department from database
+        if( response.action == "remove" ){
+            response = await inquirer.prompt([
+                { message: "Which department you want remove?", type:"input", name:"deleteDep" }
+            ])
+
+            let deleteDep = await db.query( `DELETE FROM department WHERE name='${response.deleteDep}'` )
+           
+            console.log( `Department ${response.deleteDep} has been removed.`)
+            mainApp()
+        }
+
+        if( response.action=="return" ){
+            console.log( `Returning to the main menu...`)
+            mainApp()
+        }
+    }
+    
+    if( response.action=="role"){
+        //Display role list
+        let roleList = await db.query( "SELECT * FROM role" )
+        console.table( roleList )
+
+        //Prompt user for further action for role info
+        response = await inquirer.prompt([
+            { message:"What do you want to do now?", type:"list", name:"action", 
+            choices:[
+                { name: "Add a role", value: "add" },
+                { name: "Remove a role", value: "remove" },
+                { name: "Return to main menu", value: "return" }
+            ]}
+        ])
+        //Add new role to database
+        if( response.action == "add" ){
+            const dbDepartment = await db.query( "SELECT * FROM department")
+            department = []
+            dbDepartment.forEach( (item) => {
+                department.push( { name: item.name, value: item.id } )
+            })
+
+            response = await inquirer.prompt([
+                { message: "What is the name of new role?", type:"input", name:"name" },
+                { message: "What is the salary of new role?", type:"input", name:"salary" },//validate
+                { message: "Which department does the new role belong to?", type:"list", name:"department",
+                    choices: department },
+            ])
+        
+            let newRole = await db.query( "INSERT INTO role VALUES( ?, ?, ?, ?)", 
+            [ 0, response.name, response.salary, response.department ] )
+            
+            console.log( `Role ${response.name} has been added to database.`)
+            mainApp()
+        }
+        //Remove role from database
+        if( response.action == "remove" ){
+            const dbRole = await db.query( "SELECT * FROM role")
+            role = []
+            dbRole.forEach( (item) => {
+                role.push( { name: item.title, value: item.title } )
+            })
+
+            response = await inquirer.prompt([
+                { message: "Which role you want remove?", type:"list", name:"deleteRole",
+                    choices: role }
+            ])
+
+            let deleteRole = await db.query( `DELETE FROM role WHERE title='${response.deleteRole}'` )
+           
+            console.log( `Role ${response.deleteRole} has been removed.`)
+            mainApp()
+        }
+
+        if( response.action=="return" ){
+            console.log( `Returning to the main menu...`)
+            mainApp()
+        }
+    }
+
+
 }
+mainApp()
