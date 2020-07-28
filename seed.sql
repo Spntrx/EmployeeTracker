1910ec307a8559a4a2b4a7008dbcebc5047849f5
Avi Balsingh <lemonlimewine@gmail.com>
2020-07-28 10:33:57 -0400

    Added Seed file

---
+++ seed.sql
@@ -0,0 +1,13 @@
+INSERT INTO department VALUE (1, "President" );
+INSERT INTO department VALUE (2, "HR" ); 
+INSERT INTO department VALUE (3, "Engineer" ); 
+INSERT INTO department VALUE (4, "Sales" );
+
+INSERT INTO role VALUES(1,"Senior Developer", "50000.00", "1");  
+INSERT INTO role VALUES(2,"Junior Developer", "30000.00", "2");   
+INSERT INTO role VALUES(3,"FrontEnd Developer", "40000.00", "3"); 
+INSERT INTO role VALUES(4,"BackEnd Developer", "40000.00", "4");    
+
+INSERT INTO employee VALUES(1,"Employee1", "Lastname1", 1, 1 ); 
+INSERT INTO employee VALUES(0,"Employee2", "Lastname2", 1, 1 ); 
+INSERT INTO employee VALUES(0,"Employee3", "Lastname3", 1, 1 );  
